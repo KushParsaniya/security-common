6b88cdcd513e35102dafc5bd1f326561da55f776
Kush Parsaniya <kushparsaniya94@gmail.com>
2025-09-13 12:27:20 +0530

    feat(config): add conditional property for JWT customizer

--- src/main/java/dev/kush/securitycommon/common/JwtCustomizerConfig.java
+++ src/main/java/dev/kush/securitycommon/common/JwtCustomizerConfig.java
@@ -3,6 +3,7 @@ package dev.kush.securitycommon.common;
 import com.nimbusds.jose.JOSEObjectType;
 import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.JwkSetUriJwtDecoderBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 public class JwtCustomizerConfig {
 
     @Bean
+    @ConditionalOnProperty(name = "identity.provider", havingValue = "auth0", matchIfMissing = true)
     public JwkSetUriJwtDecoderBuilderCustomizer customizer() {
         return builder -> builder.jwtProcessorCustomizer(processor ->
             processor.setJWSTypeVerifier(new DefaultJOSEObjectTypeVerifier<>(new JOSEObjectType("at+jwt")))
